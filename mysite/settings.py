d90c5adb1d7aea2ba299f42f0a3282b58e258720
kogyan <kogyan23@naver.com>
2024-11-05 18:08:32 +0900

    c

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,3 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+]
